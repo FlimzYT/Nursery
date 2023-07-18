a17b86e8b03c4c7044c90a5eec5d9925e0b2a8ce
FlimzYT <50701792+FlimzYT@users.noreply.github.com>
2023-07-18 08:48:26 +0400

    Add files via upload

---
+++ Js/events.js
@@ -0,0 +1,90 @@
+function createDate(from, to){
+
+    if(from === to){
+      return `<span>${from}</span>`
+    }
+    else{
+      return `<span>${from}</span><span>حتى</span><span>${to}</span>`
+    }
+
+}
+function createLayout(title, from, to, text, image) {
+  
+  return `
+
+    <div class="event">
+      <div class="event-info">
+          <h2>${title}</h2>
+          <div class="date">
+            ${createDate(from, to)}
+          </div>
+      </div>
+
+      <div class="event-popup hidden">
+          <div>
+              <div class="event-popup-text">
+                  <div>
+                  <h2>${title}</h2>
+                      <div class="date">
+                        ${createDate(from, to)}
+                      </div>
+                  </div>
+                  <p>${text}</p>
+              </div>
+              <img src="images/photos/${image}" alt="" class="image">
+
+              <button class="close-btn">
+                  <img src="images/misc/x.svg">
+              </button>
+          </div>
+      </div>
+  </div>
+  `
+
+}
+
+fetch('../content/eventsContent.json')
+  .then(response => response.json())
+  .then(data => {
+
+    document.querySelector('.events').innerHTML = '';
+    
+    data.forEach(event => {
+      document.querySelector('.events').innerHTML += createLayout(
+        event.arTitle,
+        event.from,
+        event.to,
+        event.arText,
+        event.image
+      );
+    });
+
+
+
+    // POPUP SCRIPT
+    const events = document.querySelectorAll('.event');
+
+    events.forEach(event => {
+      const eventPopup = event.querySelector(".event-popup");
+      if (eventPopup) {
+        const close = eventPopup.querySelector(".close-btn");
+    
+        close.addEventListener("click", (event) => {
+          event.stopPropagation();
+          eventPopup.classList.add("hidden");
+        });
+      }
+    
+      event.addEventListener("click", () => {
+        if (eventPopup) {
+          eventPopup.classList.remove("hidden");
+        }
+      });
+    });
+    
+
+    
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
